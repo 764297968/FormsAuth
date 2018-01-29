5d61f7f9034fd366873e5c9dea97ca571bbc7c8d
王成飞 <764297968@qq.com>
2018-01-29 10:59:06 +0800

    Forms多站点认证

---
+++ Auth1/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Auth1.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string name)
+        {
+            FormsAuthentication.SignOut(); 
+            FormsAuthentication.Initialize();
+            FormsAuthentication.RedirectFromLoginPage(name, false);
+            return Redirect("http://localhost:5194/");
+        }
+    }
+}
